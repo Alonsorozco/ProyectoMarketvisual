94a3e4775bed40a7737c0c1908247c2313107f10
Alonsorozco <83556353+Alonsorozco@users.noreply.github.com>
2021-11-28 19:28:36 -0300

    radd migration model categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_28_221711) do
+ActiveRecord::Schema.define(version: 2021_11_28_221922) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,7 +38,9 @@ ActiveRecord::Schema.define(version: 2021_11_28_221711) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "publication_id"
+    t.integer "user_id"
     t.index ["publication_id"], name: "index_coments_on_publication_id"
+    t.index ["user_id"], name: "index_coments_on_user_id"
   end
 
   create_table "publications", force: :cascade do |t|
