face1faeb1965c5acf61f9a314c6880650da1f91
Alonsorozco <83556353+Alonsorozco@users.noreply.github.com>
2021-11-28 19:20:52 -0300

    add migration  user references and coments

---
+++ db/migrate/20211128221922_add_user_ref_to_coments.rb
@@ -0,0 +1,5 @@
+class AddUserRefToComents < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :coments, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_28_220653) do
+ActiveRecord::Schema.define(version: 2021_11_28_221711) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2021_11_28_220653) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "coments", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "publication_id"
+    t.index ["publication_id"], name: "index_coments_on_publication_id"
   end
 
   create_table "publications", force: :cascade do |t|
